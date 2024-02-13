bcd45f9defe0121a39f53769125bb7d097c7e89e
khurshh1999 <kmkrocks787@gmail.com>
2024-02-13 10:22:21 -0500

    13/2

--- assignment-movie-database/src/main/scala/edu/neu/coe/csye7200/asstmd/Movie.scala
+++ assignment-movie-database/src/main/scala/edu/neu/coe/csye7200/asstmd/Movie.scala
@@ -4,48 +4,48 @@ import scala.io.Source
 import scala.util._
 
 /**
-  * This class represents a Movie from the IMDB data file on Kaggle.
-  * Although the limitation on 22 fields in a case class has partially gone away, it's still convenient to group the different attributes together into logical classes.
-  *
-  * Created by scalaprof on 9/12/16.
-  *
-  * Common questions in this assignment:
-  * 1. Where is main method?
-  * In most case, you don't need to run main method for assignments.
-  * Unit tests are provided to test your implementation.
-  * In this assignment, you will find the `object Movie extends App`,
-  * the `App` trait can be used to quickly turn objects into executable programs.
-  * You can read the official doc of Scala for more details.
-  *
-  * 2. How to understand the whole program in this assignment?
-  * I won't suggest you to understand the whole program in this assignment,
-  * there are some advanced features like `implicit` which hasn't been covered in class.
-  * You should be able to understand it before midterm.
-  * I will suggest you only focus on TO BE IMPLEMENTED fragments in the assignments.
-  *
-  */
+ * This class represents a Movie from the IMDB data file on Kaggle.
+ * Although the limitation on 22 fields in a case class has partially gone away, it's still convenient to group the different attributes together into logical classes.
+ *
+ * Created by scalaprof on 9/12/16.
+ *
+ * Common questions in this assignment:
+ * 1. Where is main method?
+ * In most case, you don't need to run main method for assignments.
+ * Unit tests are provided to test your implementation.
+ * In this assignment, you will find the `object Movie extends App`,
+ * the `App` trait can be used to quickly turn objects into executable programs.
+ * You can read the official doc of Scala for more details.
+ *
+ * 2. How to understand the whole program in this assignment?
+ * I won't suggest you to understand the whole program in this assignment,
+ * there are some advanced features like `implicit` which hasn't been covered in class.
+ * You should be able to understand it before midterm.
+ * I will suggest you only focus on TO BE IMPLEMENTED fragments in the assignments.
+ *
+ */
 case class Movie(title: String, format: Format, production: Production, reviews: Reviews, director: Principal, actor1: Principal, actor2: Principal, actor3: Principal, genres: Seq[String], plotKeywords: Seq[String], imdb: String)
 
 /**
-  * The movie format (including language and duration).
-  *
-  * @param color       whether filmed in color
-  * @param language    the native language of the characters
-  * @param aspectRatio the aspect ratio of the film
-  * @param duration    its length in minutes
-  */
+ * The movie format (including language and duration).
+ *
+ * @param color       whether filmed in color
+ * @param language    the native language of the characters
+ * @param aspectRatio the aspect ratio of the film
+ * @param duration    its length in minutes
+ */
 case class Format(color: Boolean, language: String, aspectRatio: Double, duration: Int) {
   override def toString = s"${if (color) "Color" else "B&W"},$language,$aspectRatio,$duration"
 }
 
 /**
-  * The production: its country, year, and financials
-  *
-  * @param country   country of origin
-  * @param budget    production budget in US dollars
-  * @param gross     gross earnings (?)
-  * @param titleYear the year the title was registered (?)
-  */
+ * The production: its country, year, and financials
+ *
+ * @param country   country of origin
+ * @param budget    production budget in US dollars
+ * @param gross     gross earnings (?)
+ * @param titleYear the year the title was registered (?)
+ */
 case class Production(country: String, budget: Int, gross: Int, titleYear: Int) {
   def isKiwi: Boolean = this match {
     case Production("New Zealand", _, _, _) => true
@@ -54,35 +54,35 @@ case class Production(country: String, budget: Int, gross: Int, titleYear: Int) 
 }
 
 /**
-  * Information about various forms of review, including the content rating.
-  */
+ * Information about various forms of review, including the content rating.
+ */
 case class Reviews(imdbScore: Double, facebookLikes: Int, contentRating: Rating, numUsersReview: Int, numUsersVoted: Int, numCriticReviews: Int, totalFacebookLikes: Int)
 
 /**
-  * A cast or crew principal
-  *
-  * @param name          name
-  * @param facebookLikes number of FaceBook likes
-  */
+ * A cast or crew principal
+ *
+ * @param name          name
+ * @param facebookLikes number of FaceBook likes
+ */
 case class Principal(name: Name, facebookLikes: Int) {
   override def toString = s"$name ($facebookLikes likes)"
 }
 
 /**
-  * A name of a contributor to the production
-  *
-  * @param first  first name
-  * @param middle middle name or initial
-  * @param last   last name
-  * @param suffix suffix
-  */
+ * A name of a contributor to the production
+ *
+ * @param first  first name
+ * @param middle middle name or initial
+ * @param last   last name
+ * @param suffix suffix
+ */
 case class Name(first: String, middle: Option[String], last: String, suffix: Option[String]) {
   override def toString = s"$first ${middle.getOrElse("")} $last ${suffix.getOrElse("")}}"
 }
 
 /**
-  * The US rating
-  */
+ * The US rating
+ */
 case class Rating(code: String, age: Option[Int]) {
   override def toString: String = code + age.map("-" + _).getOrElse("")
 }
@@ -98,14 +98,45 @@ object Movie extends App {
      * @param w a line of input.
      * @return a Try[Movie]
      */
-    def parse(w: String): Try[Movie] = {
-// TO BE IMPLEMENTED 
+    def parse(w: String): Try[Movie] = Try {
+      val parts = w.split(",").map(_.trim)
+      val title = parts(11)
+      val color = parts(0) == "Color"
+      val language = parts(19)
+      val aspectRatio = parts(26).toDouble
+      val duration = parts(3).toInt
+      val formatParams = List(parts(0), parts(19), parts(26), parts(3))
+      // Assuming `Format.apply(List[String])` correctly interprets the first element for color
+      val format = Format(formatParams)
 
-      // STUB
-       Try(???)
-      // END
-    }
-  }
+      val country = parts(20)
+      val budget = parts(22).toInt
+      val gross = parts(8).toInt
+      val titleYear = parts(23).toInt
+      val productionParams = List(parts(20), parts(22), parts(8), parts(23))
+      val production = Production(productionParams)
+
+      val imdbScore = parts(25).toDouble
+      val facebookLikes = parts(27).toInt
+      val contentRating = parts(21)
+      val numUsersReview = parts(18).toInt
+      val numUsersVoted = parts(12).toInt
+      val numCriticReviews = parts(2).toInt
+      val totalFacebookLikes = parts(13).toInt
+      val reviewsParameters = List(parts(25), parts(27), parts(21), parts(18), parts(12), parts(2), parts(13))
+      val reviewsObject = Reviews(reviewsParameters)
+
+      val director = Principal(parts(1), parts(4).toInt)
+      val actor1 = Principal(parts(10), parts(7).toInt)
+      val actor2 = Principal(parts(6), parts(24).toInt)
+      var actor3 = Principal(parts(14), parts(5).toInt)
+
+      val genres = parts(9).split("\\|").toSeq
+      val plotKeywords = parts(16).split("\\|").toSeq
+      val imdb = parts(17)
+
+      Movie(title, format, production, reviewsObject, director, actor1, actor2, actor3, genres, plotKeywords, imdb)
+    }}
 
   val ingester = new Ingest[Movie]()
   if (args.length > 0) {
@@ -116,31 +147,31 @@ object Movie extends App {
   }
 
   /**
-    * Form a list from the elements explicitly specified (by position) from the given list
-    *
-    * @param list    a list of Strings
-    * @param indices a variable number of index values for the desired elements
-    * @return a list of Strings containing the specified elements in order
-    */
+   * Form a list from the elements explicitly specified (by position) from the given list
+   *
+   * @param list    a list of Strings
+   * @param indices a variable number of index values for the desired elements
+   * @return a list of Strings containing the specified elements in order
+   */
   def elements(list: Seq[String], indices: Int*): List[String] = {
     // Hint: form a new list which is consisted by the elements in list in position indices. Int* means array of Int.
     // 6 points
     val result: Seq[String] = {
-      // TO BE IMPLEMENTED 
+      // TO BE IMPLEMENTED
 
       // STUB
-       ???
+      indices.map(list(_)).toList
       // END
     }
     result.toList
   }
 
   /**
-    * Alternative apply method for the Movie class
-    *
-    * @param ws a sequence of Strings
-    * @return a Movie
-    */
+   * Alternative apply method for the Movie class
+   *
+   * @param ws a sequence of Strings
+   * @return a Movie
+   */
   def apply(ws: Seq[String]): Movie = {
     // we ignore faceNumber_in_poster since I have no idea what that means.
     val title = ws(11)
@@ -212,15 +243,14 @@ object Rating {
   // Hint: This should similar to apply method in Object Name. The parameter of apply in case match should be same as case class Rating
   // 13 points
   def apply(s: String): Rating = {
-// TO BE IMPLEMENTED 
+    // TO BE IMPLEMENTED
+    val parts = s.split("-").toList
+    parts match {
+      case code :: Nil => Rating(code, None)
+      case code :: age :: Nil => Rating(code, Some(age.toInt))
+      case _ => throw ParseException(s"Invalid rating format: $s")
+    }
 
-
-
-
-
-    // STUB
-     ???
-    // END
   }
 }
 
